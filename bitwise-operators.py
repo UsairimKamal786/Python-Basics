25b062871d264278482fcfb0683192f61e54f864
Usairim Kamal <usairimkamal2016@gmail.com>
2025-06-27 09:36:26 +0500

    Create bitwise-operators.py

---
+++ bitwise-operators.py
@@ -0,0 +1,9 @@
+a = 10
+b = -10
+print("a >> 1 =", a >> 1)  # Right shift
+print("b >> 1 =", b >> 1)  # Left shift
+
+a = 5
+b = -10
+print("a << 1 =", a << 1)  # Left shift
+print("b << 1 =", b << 1)  # Left shift
