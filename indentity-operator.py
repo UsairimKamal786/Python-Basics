c04d351e5b0a8b1a3c1fea7c2b58545067175fcf
Usairim Kamal <usairimkamal2016@gmail.com>
2025-06-27 09:31:23 +0500

    Create indentity-operator.py

---
+++ indentity-operator.py
@@ -0,0 +1,23 @@
+x = 5
+if (type(x) is int): 
+    print("True")
+else:
+    print("False")
+
+
+
+x = 5.0
+if (type(x) is not float): 
+    print("True")
+else:
+    print("False")
+
+
+x = 20
+y = 20
+if (x is y): 
+    print("x & y  Same Indentity")
+y = 30
+if (x is not y):
+    print("x & y have Different Indentity")
+  
