0c0f6b81f5eca41cf2487dac771d14fda31fb5df
Usairim Kamal <usairimkamal2016@gmail.com>
2025-07-05 09:27:09 +0500

    Create operators_preciders.py

---
+++ operators_preciders.py
@@ -0,0 +1,16 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+
+z = (v+w) * x / y
+print("The value of (v+w) * x/y is:", z)
+
+name = "Herobrine"
+age = 0
+
+if name == "Herobrine" or name == "Technoblade" and age == 2:
+    print("Hello! Welcome to the world of Roblox!") 
+else:
+    print("Good Bye! You are not a recognized player.")
